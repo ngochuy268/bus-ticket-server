9cfb2aecce8c36514a1dc2df2c399996995742ec
ngochuy268 <huyduclong2642000@gmail.com>
2025-01-05 21:23:40 +0700

    first

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,14 +1,28 @@
 const emailModel = require('../models/emailModel');
+const axios = require('axios');
 
 const emailController = {
     sendEmail: async (req, res) => {
-        const { name, email, subject, message } = req.body;
+        const { name, email, subject, message, captchaToken  } = req.body;
 
-        if (!name || !email || !subject || !message) {
-            return res.status(400).send('All fields are required');
+        if (!name || !email || !subject || !message || !captchaToken) {
+            return res.status(400).send('All fields are required, including CAPTCHA.');
         }
 
         try {
+
+            const secretKey = '6LefuK4qAAAAACRcqvDNeBWpbolAJPO3PatKFiz_';
+            const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify`, null, {
+                params: {
+                    secret: secretKey,
+                    response: captchaToken,
+                },
+            });
+
+            if (!response.data.success) {
+                return res.status(400).json({ error: 'CAPTCHA verification failed' });
+            }
+
             await emailModel.sendEmail({ name, email, subject, message });
             res.status(200).send('Email sent successfully');
         } catch (error) {
