012b2918facbb90948d06391f41f0c075a62f1fc
ngochuy268 <huyduclong2642000@gmail.com>
2025-01-24 08:44:38 +0700

    first

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,21 +1,21 @@
 const mysql = require('mysql2');
 
 const dbConfig = {
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    // port: process.env.DB_PORT,
-    // connectionLimit: 5,
-    // waitForConnections: true,
-    // ssl: {
-    //   rejectUnauthorized: false
-    // }
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    connectionLimit: 5,
+    waitForConnections: true,
+    ssl: {
+      rejectUnauthorized: false
+    }
   
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'busticket'
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'busticket'
 };
 
 const connection = mysql.createPool(dbConfig);
